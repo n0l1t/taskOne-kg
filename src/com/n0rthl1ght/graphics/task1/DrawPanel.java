066c46ffbd3cef1399b1487d5b967a78c00cf79b
n0l1t <n0rthl1ght@yandex.ru>
2023-12-30 11:36:46 +0300

    add background

--- src/com/n0rthl1ght/graphics/task1/DrawPanel.java
+++ src/com/n0rthl1ght/graphics/task1/DrawPanel.java
@@ -1,14 +1,30 @@
 package com.n0rthl1ght.graphics.task1;
 
+import com.n0rthl1ght.graphics.task1.elements.Background;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class DrawPanel extends JPanel {
+
+    private int partWidth;
+    private int partHeight;
+
+    private Background bg;
     public DrawPanel() {
+        bg = new Background(getWidth(), 0, getWidth(), getHeight());
     }
 
     @Override
-    public void paint(Graphics g) {
-        super.paint(g);
+    public void paint(Graphics gr) {
+        super.paint(gr);
+        Graphics2D g = (Graphics2D) gr;
+
+        partHeight = getHeight();
+        partWidth = getWidth();
+
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        bg.draw(g,partWidth,partHeight);
     }
 }
