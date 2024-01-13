2fc64af71c219dec3c3d7f0b0937f550a869976c
n0l1t <n0rthl1ght@yandex.ru>
2024-01-13 23:29:39 +0300

    wave logic rework

--- src/com/n0rthl1ght/graphics/task1/DrawPanel.java
+++ src/com/n0rthl1ght/graphics/task1/DrawPanel.java
@@ -6,16 +6,27 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class DrawPanel extends JPanel {
-
+    private Random rnd = new Random();
     private Background bg;
     private Sea sea;
     private List<Mountain> mountains;
-    private List<Wave> wave;
+    private List<Wave> wv;
     public DrawPanel() {
         bg = new Background();
         sea = new Sea(0,450);
+
+        wv = new ArrayList<Wave>();
+
+        for (int i = 20; i < 700; i=i+rnd.nextInt(10,50)) {
+            int startX = i;
+            int startY = rnd.nextInt(455, 750);
+            int endX = startX+rnd.nextInt(30,100);
+            int endY=startY;
+            wv.add(WavesCreator.createWave(startX,startY,endX,endY));
+        }
 
         mountains = new ArrayList<Mountain>();
         mountains.add(MountainCreator.createM1());
@@ -24,12 +35,6 @@ public class DrawPanel extends JPanel {
         mountains.add(MountainCreator.createM4());
         mountains.add(MountainCreator.createM5());
 
-        wave = new ArrayList<Wave>();
-        wave.add((WavesCreator.createW1()));
-        wave.add((WavesCreator.createW2()));
-        wave.add((WavesCreator.createW3()));
-        wave.add((WavesCreator.createW4()));
-        wave.add((WavesCreator.createW5()));
 
     }
 
@@ -42,9 +47,11 @@ public class DrawPanel extends JPanel {
 
         bg.draw(g,getWidth(),getHeight());
         sea.draw(g,getWidth(),getHeight());
-        for(Wave i: wave){
-            i.draw(g,getWidth(),getHeight());
+
+        for(Wave i: wv){
+            i.draw(g, getWidth(),getHeight());
         }
+
         for(Mountain i: mountains){
             i.draw(g, getWidth(),getHeight());
         }
--- src/com/n0rthl1ght/graphics/task1/elements/Wave.java
+++ src/com/n0rthl1ght/graphics/task1/elements/Wave.java
@@ -7,18 +7,18 @@ public class Wave {
     private int startY;
     private int endX;
     private int endY;
-    private Color color;
 
-    public Wave(int startX, int startY, int endX, int endY, Color color) {
+    private Color col = new Color(83,83,83);
+
+    public Wave(int startX, int startY, int endX, int endY) {
         this.startX = startX;
         this.startY = startY;
         this.endX = endX;
         this.endY = endY;
-        this.color=color;
     }
 
     public void draw(Graphics2D g, int w, int h){
-        g.setPaint(color);
+        g.setPaint(col);
         g.drawLine(startX,startY,endX,endY);
     }
 }
